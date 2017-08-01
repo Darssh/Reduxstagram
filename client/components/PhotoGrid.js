7ce3a98f3d50a74df4c51bc0fda6c3d5fcbd41bb
Darssh <darshitsuthar91@gmail.com>
2017-08-01 13:05:14 -0700

    delete commet reducer added

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ class PhotoGrid extends React.Component {
     return (
       <div className="photo-grid">
         {this.props.posts.map((post, i) => {
-          return <Photo key={i} i={i} {...this.props} post={post} />
+          return <Photo key={i} i={i} post={post} {...this.props} />
         })}
       </div>
     );
